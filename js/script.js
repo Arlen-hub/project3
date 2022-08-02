bc5fd844e624e3a0dab91fc233e96e2b9ff0ef09
Arlen <arlen.ko3@gmail.com>
2022-08-02 12:27:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+// const result = confirm("are you here?");
+
+// console.log(result);
+
+// const answr = prompt("Вам уже есть 18", "");
+// console.log(answr + 5);
+
+// const arr = [];
+
+// arr [0] = prompt("whats your name ?", "");
+// arr [1] = prompt("whats your last name ?", "");
+// arr [2] = prompt("H=how old are you ?", "");
+
+// document.write
+// console.log(typeof(arr));
+
+// const ans = "Arlen";
+
+// alert(`hello, ${ans}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+
+// const der = true,
+//       bar = false;
+
+// console.log(!der || bar);
+
+console.log(2 + 2 * 2 == '6' );
