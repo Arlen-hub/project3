89cd49ce38d2e7faa337b47c577ccf015f00f7ec
Arlen <arlen.ko3@gmail.com>
2022-08-03 12:58:52 +0300

    Первое приложение

--- js/script.js
+++ js/script.js
@@ -1,37 +1,19 @@
-
-// const result = confirm("are you here?");
-
-// console.log(result);
-
-// const answr = prompt("Вам уже есть 18", "");
-// console.log(answr + 5);
-
-// const arr = [];
-
-// arr [0] = prompt("whats your name ?", "");
-// arr [1] = prompt("whats your last name ?", "");
-// arr [2] = prompt("H=how old are you ?", "");
-
-// document.write
-// console.log(typeof(arr));
-
-// const ans = "Arlen";
-
-// alert(`hello, ${ans}`);
-
-// let incr = 10,
-//     decr = 10;
-
-// // incr++;
-// // decr--;
-
-// console.log(++incr);
-// console.log(--decr);
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
 
 
-// const der = true,
-//       bar = false;
 
-// console.log(!der || bar);
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies :{
+    },
+    actors :{
+    },
+    genres :[],
+    privat : false
+};
 
-console.log(2 + 2 * 2 == '6' );
+const firstQuation = prompt('Один из послдених просмотренных фильмов?', '');
+
+const secondQuation = +prompt('На сколько его оцените?', '');
+personalMovieDB.movies[firstQuation] = secondQuation;
+console.log(personalMovieDB);
